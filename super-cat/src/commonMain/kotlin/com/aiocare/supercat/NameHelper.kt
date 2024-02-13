60a73d4543caa32abfe85cf5e977ecf7ec477d95
Szymon Kamiński <szymon.kaminski@proexe.pl>
2024-02-13 22:44:41 +0100

    ktlint

--- super-cat/src/commonMain/kotlin/com/aiocare/supercat/NameHelper.kt
+++ super-cat/src/commonMain/kotlin/com/aiocare/supercat/NameHelper.kt
@@ -2,7 +2,7 @@ package com.aiocare.supercat
 
 object NameHelper {
 
-    fun parse(input: String): String = when(input.contains("@")){
+    fun parse(input: String): String = when (input.contains("@")) {
         true -> input.split("@").last()
         false -> input
     }
